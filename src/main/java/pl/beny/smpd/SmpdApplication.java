01ec330128bd7e34d4c3c8f3ce7350d711b7a99b
Kamil Wasilewski <bbeny123@gmail.com>
2018-06-16 19:20:16 +0200

    init

---
+++ src/main/java/pl/beny/smpd/SmpdApplication.java
@@ -0,0 +1,12 @@
+package pl.beny.smpd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmpdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmpdApplication.class, args);
+    }
+}
