e13c1ca91ceb63ec36eac4d4902b80e5a2615b5c
Kamil Wasilewski <bbeny123@gmail.com>
2018-06-18 03:05:47 +0200

    optimization

--- src/main/java/pl/beny/smpd/Database.java
+++ src/main/java/pl/beny/smpd/Database.java
@@ -1,9 +1,7 @@
 package pl.beny.smpd;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -12,6 +10,8 @@ public class Database {
     public static final String ACER = "Acer";
     public static final String QUERCUS = "Quercus";
     private static final List<Sample> samples = new ArrayList<>();
+    private static final List<Sample> acerSamples = new ArrayList<>();
+    private static final List<Sample> quercusSamples = new ArrayList<>();
 
     private static Database instance = new Database();
 
@@ -30,14 +30,13 @@ public class Database {
             String arr[] = in.nextLine().split(",", 2);
             samples.add(new Sample(arr[0].split(" ")[0], Stream.of(arr[1].split(",")).map(Double::parseDouble).collect(Collectors.toList())));
         }
+
+        acerSamples.addAll(getSamples(samples, ACER));
+        quercusSamples.addAll(getSamples(samples, QUERCUS));
     }
 
     static List<Sample> getSamples() {
         return samples;
-    }
-
-    static List<Sample> getSamples(String className) {
-        return samples.stream().filter(s -> className.equals(s.getClassName())).collect(Collectors.toList());
     }
 
     static List<Sample> getSamples(List<Sample> samples, String className) {
@@ -47,11 +46,14 @@ public class Database {
     static List<List<Double>> getByIndexes(String className, List<Integer> indexes) {
         return indexes
                 .stream()
-                .map(i -> samples.stream()
-                        .filter(s -> className.equals(s.getClassName()))
+                .map(i -> getSamples(className).stream()
                         .map(a -> a.getAttr().get(i))
                         .collect(Collectors.toList()))
                 .collect(Collectors.toList());
+    }
+
+    static List<Sample> getSamples(String className) {
+        return className.equals(ACER) ? acerSamples : quercusSamples;
     }
 
     static List<List<Double>> getByIndexes(List<Sample> samples, List<Integer> indexes) {
--- src/main/java/pl/beny/smpd/Main.java
+++ src/main/java/pl/beny/smpd/Main.java
@@ -5,25 +5,25 @@ public class Main {
     public static void main(String[] args) {
         long startTime;
 
+//        startTime = System.nanoTime();
+//        System.out.println(new SFS().computeSFSP(64));
+//        System.out.println((System.nanoTime() - startTime) / 1000000);
+//
         startTime = System.nanoTime();
-        System.out.println(new SFS().computeSFSP(2));
+        System.out.println(new Fisher().computeFisherP(5));
         System.out.println((System.nanoTime() - startTime) / 1000000);
-
-        startTime = System.nanoTime();
-        System.out.println(new Fisher().computeFisherP(2));
-        System.out.println((System.nanoTime() - startTime) / 1000000);
-
-        startTime = System.nanoTime();
-        System.out.println(Classificators.classifyNN(Database.getSamples(Database.ACER).get(1), Database.getSamples()));
-        System.out.println((System.nanoTime() - startTime) / 1000000);
-
-        startTime = System.nanoTime();
-        Crossvalidation.checkCrossvalidation(6, 5);
-        System.out.println((System.nanoTime() - startTime) / 1000000);
-
-        startTime = System.nanoTime();
-        Bootstrap.checkBootstrap(20, 50, 4);
-        System.out.println((System.nanoTime() - startTime) / 1000000);
+//
+//        startTime = System.nanoTime();
+//        System.out.println(Classificators.classifyNN(Database.getSamples(Database.ACER).get(1), Database.getSamples()));
+//        System.out.println((System.nanoTime() - startTime) / 1000000);
+//
+//        startTime = System.nanoTime();
+//        Crossvalidation.checkCrossvalidation(6, 3);
+//        System.out.println((System.nanoTime() - startTime) / 1000000);
+//
+//        startTime = System.nanoTime();
+//        Bootstrap.checkBootstrap(30, 200, 6);
+//        System.out.println((System.nanoTime() - startTime) / 1000000);
     }
 
 }
