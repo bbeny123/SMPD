42da0959c27ce9bdcb3f4ef2fd6256a94da527bc
Kamil Wasilewski <bbeny123@gmail.com>
2018-06-18 03:54:57 +0200

    clean up

--- src/main/java/pl/beny/smpd/Main.java
+++ src/main/java/pl/beny/smpd/Main.java
@@ -5,17 +5,17 @@ public class Main {
     public static void main(String[] args) {
         long startTime;
 
-//        startTime = System.nanoTime();
-//        System.out.println(new SFS().computeSFSP(64));
-//        System.out.println((System.nanoTime() - startTime) / 1000000);
-//
-//        startTime = System.nanoTime();
-//        System.out.println(new Fisher().computeFisherP(5));
-//        System.out.println((System.nanoTime() - startTime) / 1000000);
-//
-//        startTime = System.nanoTime();
-//        System.out.println(Classificators.classifyNN(Database.getSamples(Database.ACER).get(1), Database.getSamples()));
-//        System.out.println((System.nanoTime() - startTime) / 1000000);
+        startTime = System.nanoTime();
+        System.out.println(new SFS().computeSFSP(64));
+        System.out.println((System.nanoTime() - startTime) / 1000000);
+
+        startTime = System.nanoTime();
+        System.out.println(new Fisher().computeFisherP(4));
+        System.out.println((System.nanoTime() - startTime) / 1000000);
+
+        startTime = System.nanoTime();
+        System.out.println(Classificators.classifyNN(Database.getSamples(Database.ACER).get(1), Database.getSamples()));
+        System.out.println((System.nanoTime() - startTime) / 1000000);
 
         startTime = System.nanoTime();
         Quality.checkCrossvalidation(6, 3);
